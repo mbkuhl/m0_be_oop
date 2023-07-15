6388593f4bd372b688fb66462866e9a2d17aeb3c
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-07-14 20:22:34 -0500

    Add Unicorn class

--- oop.rb
+++ oop.rb
@@ -3,6 +3,16 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+    def initialize(name)
+        @name = name
+        @color = "silver"
+    end
+    def say(string)
+        "*~* #{string} *~*"
+    end
+end
+
 
 
 #  Write a class called Vampire
